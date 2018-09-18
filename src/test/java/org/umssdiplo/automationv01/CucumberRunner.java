a0f2f66796c70223c57af4e6954c11d6f1ee1480
Carlos Meneses <kenshinmc23@gmail.com>
2018-09-17 20:41:15 -0400

    corrigiendo configuraciones ver 24

--- src/test/java/org/umssdiplo/automationv01/CucumberRunner.java
+++ src/test/java/org/umssdiplo/automationv01/CucumberRunner.java
@@ -4,7 +4,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-        features = {"src/test/resources/features/Affliction.feature"},
+        features = {"src/test/resources/features"},
         format = {"pretty",
                 "html:build/cucumber",
                 "json:build/cucumber/cucumber.json",
